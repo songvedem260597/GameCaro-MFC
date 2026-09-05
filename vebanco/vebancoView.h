5d513d115b32a8f0f7ce9aadc510162dfa07a503
songvedem <56100101+songvedem260597@users.noreply.github.com>
2026-09-06 03:26:02 +0700

    fix: eliminate Caro UI flicker during redraw

--- vebanco/vebancoView.h
+++ vebanco/vebancoView.h
@@ -30,7 +30,7 @@ public:
 
 // Overrides
 public:
-	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
+	virtual void OnDraw(CDC* pDC);
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 protected:
 	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
@@ -53,10 +53,10 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
 };
 
-#ifndef _DEBUG  // debug version in vebancoView.cpp
+#ifndef _DEBUG
 inline CvebancoDoc* CvebancoView::GetDocument() const
    { return reinterpret_cast<CvebancoDoc*>(m_pDocument); }
 #endif
-
