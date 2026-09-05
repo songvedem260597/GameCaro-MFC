b9d4090796aa82df8b8c288d25e827403e8f984e
songvedem <56100101+songvedem260597@users.noreply.github.com>
2026-09-06 03:06:26 +0700

    ui: modernize Caro board and side panel

--- vebanco/vebancoView.cpp
+++ vebanco/vebancoView.cpp
@@ -3,8 +3,6 @@
 //
 
 #include "stdafx.h"
-// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
-// and search filter handlers and allows sharing of document code with that project.
 #ifndef SHARED_HANDLERS
 #include "vebanco.h"
 #endif
@@ -16,108 +14,305 @@
 #define new DEBUG_NEW
 #endif
 
+namespace
+{
+	const COLORREF kBackground = RGB(240, 244, 248);
+	const COLORREF kPanel = RGB(255, 255, 255);
+	const COLORREF kBoard = RGB(250, 252, 255);
+	const COLORREF kGrid = RGB(210, 218, 228);
+	const COLORREF kGridStrong = RGB(170, 181, 196);
+	const COLORREF kText = RGB(34, 42, 55);
+	const COLORREF kMuted = RGB(111, 121, 137);
+	const COLORREF kBlue = RGB(37, 99, 235);
+	const COLORREF kRed = RGB(239, 68, 68);
+	const COLORREF kGreen = RGB(16, 185, 129);
+	const COLORREF kHighlight = RGB(219, 234, 254);
+	const COLORREF kShadow = RGB(220, 226, 234);
 
-// CvebancoView
+	void GetLayout(CWnd* view, CRect& boardRect, CRect& sideRect, int& cellSize)
+	{
+		CRect client;
+		view->GetClientRect(&client);
+
+		const int margin = 28;
+		const int gap = 28;
+		const int sideWidth = 320;
+		const int maxBoard = min(client.Height() - margin * 2, client.Width() - sideWidth - gap - margin * 2);
+		cellSize = max(18, maxBoard / 30);
+		const int boardPixels = cellSize * 30;
+
+		int top = max(margin, (client.Height() - boardPixels) / 2);
+		int left = margin;
+		boardRect = CRect(left, top, left + boardPixels, top + boardPixels);
+		sideRect = CRect(boardRect.right + gap, top, min(client.right - margin, boardRect.right + gap + sideWidth), top + boardPixels);
+	}
+
+	void FillRoundRect(CDC* pDC, const CRect& rect, COLORREF color, int radius)
+	{
+		CBrush brush(color);
+		CPen pen(PS_SOLID, 1, color);
+		CBrush* oldBrush = pDC->SelectObject(&brush);
+		CPen* oldPen = pDC->SelectObject(&pen);
+		pDC->RoundRect(rect, CPoint(radius, radius));
+		pDC->SelectObject(oldPen);
+		pDC->SelectObject(oldBrush);
+	}
+}
 
 IMPLEMENT_DYNCREATE(CvebancoView, CView)
 
 BEGIN_MESSAGE_MAP(CvebancoView, CView)
-	// Standard printing commands
 	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
 	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
 	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
 	ON_WM_LBUTTONDOWN()
 END_MESSAGE_MAP()
 
-// CvebancoView construction/destruction
-
 CvebancoView::CvebancoView()
 {
-	// TODO: add construction code here
 	size = 30;
-	playercount = 0,condition = 1;
 	row = 30;
 	column = 30;
-	for (int i = 0; i < row; i++)
-		for (int j = 0; j < column; j++)
-			arr[i][j] = 0;
+	ResetGame();
 }
 
 CvebancoView::~CvebancoView()
 {
 }
 
+void CvebancoView::ResetGame()
+{
+	playercount = 0;
+	condition = 1;
+	winner = 0;
+	lastRow = -1;
+	lastColumn = -1;
+
+	for (int i = 0; i < row; i++)
+		for (int j = 0; j < column; j++)
+			arr[i][j] = 0;
+
+	if (GetSafeHwnd())
+		Invalidate(FALSE);
+}
+
 BOOL CvebancoView::PreCreateWindow(CREATESTRUCT& cs)
 {
-	// TODO: Modify the Window class or styles here by modifying
-	//  the CREATESTRUCT cs
-
+	cs.style &= ~WS_BORDER;
 	return CView::PreCreateWindow(cs);
 }
 
-// CvebancoView drawing
-void valueview(CClientDC *pDC,int arr[][max],int row,int column)
-{
-	CString text;
-	for (int i = 0; i < row; i++)
-	{
-		for (int j = 0; j < column; j++)
-		{
-			text.Format(_T("%d"), arr[i][j]);
-			pDC->TextOutW(1000 + j * 10, 100 + i * 15, text);
-		}
-	}
-}
-void CvebancoView::OnDraw(CDC* /*pDC*/)
+void CvebancoView::OnDraw(CDC* pDC)
 {
 	CvebancoDoc* pDoc = GetDocument();
 	ASSERT_VALID(pDoc);
 	if (!pDoc)
 		return;
-	CClientDC pDC(this);
+
+	CRect client;
+	GetClientRect(&client);
+	pDC->FillSolidRect(client, kBackground);
+	pDC->SetBkMode(TRANSPARENT);
+
+	CRect boardRect, sideRect;
+	int cellSize = 0;
+	GetLayout(this, boardRect, sideRect, cellSize);
+	size = cellSize;
+
+	CRect boardShadow = boardRect;
+	boardShadow.OffsetRect(6, 8);
+	FillRoundRect(pDC, boardShadow, kShadow, 18);
+	FillRoundRect(pDC, boardRect, kBoard, 18);
+
+	CRect panelShadow = sideRect;
+	panelShadow.OffsetRect(6, 8);
+	FillRoundRect(pDC, panelShadow, kShadow, 18);
+	FillRoundRect(pDC, sideRect, kPanel, 18);
+
+	// Board grid and cell hit boxes
 	for (int i = 0; i < row; i++)
 	{
-		for (int j = 0; j <column; j++)
+		for (int j = 0; j < column; j++)
 		{
-			oco[i][j].setup(j*size, i*size, size + j*size, size + i*size);
-			oco[i][j].draw(&pDC);
+			int left = boardRect.left + j * cellSize;
+			int top = boardRect.top + i * cellSize;
+			oco[i][j].setup(left, top, left + cellSize, top + cellSize);
 		}
 	}
-	
-	CString strcountplayer,strplayerwin,GameName;
-	GameName = " GAME CARO";
-	strplayerwin = "Player Win Is : ";
-	strcountplayer = "Player :";
-	pDC.TextOutW(1000, 70, GameName);
-	pDC.TextOutW(1000, 100, strplayerwin);
-	pDC.TextOutW(1000, 120, strcountplayer);
 
+	if (lastRow >= 0 && lastColumn >= 0)
+	{
+		CRect highlight(
+			boardRect.left + lastColumn * cellSize + 1,
+			boardRect.top + lastRow * cellSize + 1,
+			boardRect.left + (lastColumn + 1) * cellSize,
+			boardRect.top + (lastRow + 1) * cellSize);
+		pDC->FillSolidRect(highlight, kHighlight);
+	}
 
-	//valueview(&pDC, arr);
-	// TODO: add draw code for native data here
+	for (int n = 0; n <= row; n++)
+	{
+		COLORREF lineColor = (n % 5 == 0) ? kGridStrong : kGrid;
+		CPen pen(PS_SOLID, 1, lineColor);
+		CPen* oldPen = pDC->SelectObject(&pen);
+		int y = boardRect.top + n * cellSize;
+		pDC->MoveTo(boardRect.left, y);
+		pDC->LineTo(boardRect.right, y);
+		pDC->SelectObject(oldPen);
+	}
+
+	for (int n = 0; n <= column; n++)
+	{
+		COLORREF lineColor = (n % 5 == 0) ? kGridStrong : kGrid;
+		CPen pen(PS_SOLID, 1, lineColor);
+		CPen* oldPen = pDC->SelectObject(&pen);
+		int x = boardRect.left + n * cellSize;
+		pDC->MoveTo(x, boardRect.top);
+		pDC->LineTo(x, boardRect.bottom);
+		pDC->SelectObject(oldPen);
+	}
+
+	// Pieces
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			if (arr[i][j] == 0)
+				continue;
+
+			CRect cell(
+				boardRect.left + j * cellSize,
+				boardRect.top + i * cellSize,
+				boardRect.left + (j + 1) * cellSize,
+				boardRect.top + (i + 1) * cellSize);
+			cell.DeflateRect(max(4, cellSize / 5), max(4, cellSize / 5));
+
+			if (arr[i][j] == 1)
+			{
+				CPen pen(PS_SOLID, max(2, cellSize / 8), kRed);
+				CPen* oldPen = pDC->SelectObject(&pen);
+				pDC->MoveTo(cell.left, cell.top);
+				pDC->LineTo(cell.right, cell.bottom);
+				pDC->MoveTo(cell.right, cell.top);
+				pDC->LineTo(cell.left, cell.bottom);
+				pDC->SelectObject(oldPen);
+			}
+			else
+			{
+				CPen pen(PS_SOLID, max(2, cellSize / 8), kGreen);
+				CPen* oldPen = pDC->SelectObject(&pen);
+				CBrush* oldBrush = (CBrush*)pDC->SelectStockObject(NULL_BRUSH);
+				pDC->Ellipse(cell);
+				pDC->SelectObject(oldBrush);
+				pDC->SelectObject(oldPen);
+			}
+		}
+	}
+
+	// Side panel
+	int x = sideRect.left + 28;
+	int y = sideRect.top + 30;
+	int contentWidth = max(120, sideRect.Width() - 56);
+
+	CFont titleFont;
+	titleFont.CreatePointFont(250, _T("Segoe UI Semibold"));
+	CFont labelFont;
+	labelFont.CreatePointFont(105, _T("Segoe UI"));
+	CFont valueFont;
+	valueFont.CreatePointFont(135, _T("Segoe UI Semibold"));
+	CFont buttonFont;
+	buttonFont.CreatePointFont(110, _T("Segoe UI Semibold"));
+
+	CFont* oldFont = pDC->SelectObject(&titleFont);
+	pDC->SetTextColor(kText);
+	pDC->TextOutW(x, y, _T("CARO"));
+	y += 48;
+
+	pDC->SelectObject(&labelFont);
+	pDC->SetTextColor(kMuted);
+	pDC->TextOutW(x, y, _T("Classic 5-in-a-row"));
+	y += 62;
+
+	CRect turnCard(x, y, x + contentWidth, y + 112);
+	FillRoundRect(pDC, turnCard, RGB(247, 249, 252), 14);
+	pDC->SetTextColor(kMuted);
+	pDC->TextOutW(turnCard.left + 18, turnCard.top + 16, _T("STATUS"));
+
+	pDC->SelectObject(&valueFont);
+	CString status;
+	COLORREF statusColor = kBlue;
+	if (winner == 1)
+	{
+		status = _T("Player X wins");
+		statusColor = kRed;
+	}
+	else if (winner == 2)
+	{
+		status = _T("Player O wins");
+		statusColor = kGreen;
+	}
+	else if (playercount % 2 == 0)
+	{
+		status = _T("Player X turn");
+		statusColor = kRed;
+	}
+	else
+	{
+		status = _T("Player O turn");
+		statusColor = kGreen;
+	}
+	pDC->SetTextColor(statusColor);
+	pDC->TextOutW(turnCard.left + 18, turnCard.top + 50, status);
+	y += 138;
+
+	pDC->SelectObject(&labelFont);
+	pDC->SetTextColor(kMuted);
+	pDC->TextOutW(x, y, _T("Players"));
+	y += 34;
+
+	CRect xCard(x, y, x + contentWidth, y + 56);
+	FillRoundRect(pDC, xCard, RGB(254, 242, 242), 12);
+	pDC->SetTextColor(kRed);
+	pDC->SelectObject(&valueFont);
+	pDC->TextOutW(xCard.left + 18, xCard.top + 15, _T("X   Red"));
+	y += 68;
+
+	CRect oCard(x, y, x + contentWidth, y + 56);
+	FillRoundRect(pDC, oCard, RGB(236, 253, 245), 12);
+	pDC->SetTextColor(kGreen);
+	pDC->TextOutW(oCard.left + 18, oCard.top + 15, _T("O   Green"));
+	y += 88;
+
+	CRect newGameRect(x, y, x + contentWidth, y + 54);
+	FillRoundRect(pDC, newGameRect, kBlue, 13);
+	pDC->SelectObject(&buttonFont);
+	pDC->SetTextColor(RGB(255, 255, 255));
+	CString buttonText = _T("NEW GAME");
+	CSize buttonSize = pDC->GetTextExtent(buttonText);
+	pDC->TextOutW(newGameRect.left + (newGameRect.Width() - buttonSize.cx) / 2,
+		newGameRect.top + (newGameRect.Height() - buttonSize.cy) / 2, buttonText);
+
+	pDC->SelectObject(&labelFont);
+	pDC->SetTextColor(kMuted);
+	CString moves;
+	moves.Format(_T("Moves played: %d"), playercount);
+	pDC->TextOutW(x, sideRect.bottom - 44, moves);
+
+	pDC->SelectObject(oldFont);
 }
-
-
-// CvebancoView printing
 
 BOOL CvebancoView::OnPreparePrinting(CPrintInfo* pInfo)
 {
-	// default preparation
 	return DoPreparePrinting(pInfo);
 }
 
 void CvebancoView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 {
-	// TODO: add extra initialization before printing
 }
 
 void CvebancoView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 {
-	// TODO: add cleanup after printing
 }
-
-
-// CvebancoView diagnostics
 
 #ifdef _DEBUG
 void CvebancoView::AssertValid() const
@@ -130,68 +325,55 @@ void CvebancoView::Dump(CDumpContext& dc) const
 	CView::Dump(dc);
 }
 
-CvebancoDoc* CvebancoView::GetDocument() const // non-debug version is inline
+CvebancoDoc* CvebancoView::GetDocument() const
 {
 	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CvebancoDoc)));
 	return (CvebancoDoc*)m_pDocument;
 }
 #endif //_DEBUG
 
-
-// CvebancoView message handlers
-
-
 void CvebancoView::OnLButtonDown(UINT nFlags, CPoint point)
 {
-	int flag=0;
-	p1.x = point.x;
-	p1.y = point.y;
-	CString player,playerwin,funny;
-	funny = "Bai lam chi mang tinh chat lay lai liem si cho ban than =))";
-	//CString array;
-	player.Format(_T("%d"),playercount);
-	CClientDC pDC(this);
-	CBrush *redbrush = new CBrush(RGB(255, 0, 0));
-	CBrush *greenbrush = new CBrush(RGB(0, 255, 0));
-	for (int i = 0; i < row; i++)
+	CRect boardRect, sideRect;
+	int cellSize = 0;
+	GetLayout(this, boardRect, sideRect, cellSize);
+
+	int panelX = sideRect.left + 28;
+	int panelY = sideRect.top + 30 + 48 + 62 + 138 + 34 + 68 + 88;
+	CRect newGameRect(panelX, panelY, panelX + max(120, sideRect.Width() - 56), panelY + 54);
+	if (newGameRect.PtInRect(point))
 	{
-		for (int j = 0; j < column; j++)
-		{
-			if (oco[i][j].boxcheck(p1) == 1 && playercount % 2 == 0 && condition == 1 && arr[i][j]==0 && arr[i][j] != 2)
-			{
-				pDC.SelectObject(redbrush);
-				oco[i][j].draw(&pDC);
-				oco[i][j].drawX(&pDC);
-				pDC.TextOutW(1055, 120, player);
-				arr[i][j] = 1;
-				playercount++;
-				//array.Format(_T("%d"), oco[i][j].checkToWin(arr, i, j));
-				
-			}
-			else if (oco[i][j].boxcheck(p1) == 1 && playercount % 2 != 0 && condition == 1 && arr[i][j] == 0 && arr[i][j] != 1)
-			{
-				pDC.SelectObject(greenbrush);
-				oco[i][j].draw(&pDC);
-				oco[i][j].drawO(&pDC);
-				pDC.TextOutW(1055, 120, player);
-				arr[i][j] = 2;
-				playercount++;
-				//array.Format(_T("%d"), oco[i][j].checkToWin(arr, i, j));
-			}
-			
-			if (oco[i][j].boxcheck(p1) == 1 && oco[i][j].checkToWin(arr,i,j)==1)
-			{
-				flag = 1;
-				pDC.TextOutW(1100, 100, player);
-				pDC.TextOutW(950, 150, funny);
-			}
-		}
-		CString str, text;
+		ResetGame();
+		CView::OnLButtonDown(nFlags, point);
+		return;
 	}
-	if (flag == 1)
+
+	if (!condition || !boardRect.PtInRect(point))
 	{
+		CView::OnLButtonDown(nFlags, point);
+		return;
+	}
+
+	int col = (point.x - boardRect.left) / cellSize;
+	int r = (point.y - boardRect.top) / cellSize;
+	if (r < 0 || r >= row || col < 0 || col >= column || arr[r][col] != 0)
+	{
+		CView::OnLButtonDown(nFlags, point);
+		return;
+	}
+
+	int currentPlayer = (playercount % 2 == 0) ? 1 : 2;
+	arr[r][col] = currentPlayer;
+	lastRow = r;
+	lastColumn = col;
+	playercount++;
+
+	if (oco[r][col].checkToWin(arr, r, col) == 1)
+	{
+		winner = currentPlayer;
 		condition = 0;
 	}
-	playercount = playercount % 2;
+
+	Invalidate(FALSE);
 	CView::OnLButtonDown(nFlags, point);
 }
